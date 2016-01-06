bb20a0c92fb3193c4ce204bbe1e573441c9bf7b2
RosannevanderPol <ccroos@gmail.com>
2016-01-06 12:24:48 +0100

    prototype

---
+++ app/src/main/java/com/rosanne/taal/oefcat.java
@@ -0,0 +1,13 @@
+package com.rosanne.taal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class oefcat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oefcat);
+    }
+}
